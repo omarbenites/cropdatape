41c334cedfd2900cbf8d21d55324c819989f49c5
omarbenites <omarbenites@github.com>
2017-01-11 12:10:05 -0500

    #script para crear data depurada

--- R/utils.R
+++ R/utils.R
@@ -10,98 +10,128 @@ library(stringr)
 fp2 <- file.choose()
 
 # Leemos el archivo
-dc <-  read.xlsx(xlsxFile = fp2, sheet = 1,skipEmptyRows = TRUE)
+dc <-  read.xlsx(xlsxFile = fp2, sheet = 1, skipEmptyRows = TRUE)
+
+dct <- dc %>% filter(variable  != "Department",
+                      variable != "Nacional" ,
+                      variable != "NA")
 
 # Vemos la estructura, y se observa que la ultima columna, esta vacia.
-str(dc) 
+# Por defecto todas las cabezeras salen con nombres X3 hasta X14
+str(dct) 
 dc$X15
-
 # data.frame':	160 obs. of  15 variables:  Eliminamos la variable 15 (variable vacia)
-dc <- dc[,-15]
-
+dct <- dct[,-15]
 # Eliminamos la columna X2 (El Total) -------------------------------------
-dc <- dc[,-2]
+dct <- dct[,-2]
 
-#dt <- dt %>% complete.cases(.)
-dct <- dc %>% gather (month, value, X3:X14)
 
-# Eliminamos filas con characteres inutiles.
-#p <- c("sowa","Nacional","Department", "harva", "production", "yield", "pricePlot")
-dct <- dct %>% filter(variable != "Department", 
-                      variable != "Nacional",
-                      variable != "sowa",
-                      variable != "harva",
-                      variable != "production", 
-                      variable != "yield",
-                      variable != "pricePlot")
+# Sowa: Sowing by area --------------------------------------------------------------------
+# Seleccionamos las filas 30 primeras filas (del 1 al 30)
+
+dtsowa <- dplyr::slice(dct, 1:30)
+csowa <- c("department", "Ago", "Set", "Oct", "Nov", "Dic", "Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul")
+#asignar los nombres
+names(dtsowa) <- csowa 
+
+# Funcion 'gather(data, key, value)' (paquete tidyr): sirve para transponer las tablas 
+# de forma ordenada.
+# Parametros o inputs dela funcion:
+# data: los datos a usar.
+# key: la columna que va almacenar las cabezeras.
+# value: la columna que va almacenar los valores de cada cabezera. Se usa un 'rango'.
+
+head(dtsowa)
+dtsowa <- gather (dtsowa, month, sowa, Ago:Jul)
+
+# Todavia existe un valor que esta sobrando
+dtsowa <- dtsowa %>% filter( department!= "sowa") %>% arrange(department,month)
 
 # Observamos la tabla 'dct', y las tablas empiezan con Tumbes y terminan con Madre de Dios.
 
 # Creamos el vector 'tvar',  el cual contiene los atributos de los cultivos 
-tvar <- c("sowa","harva", "production", "yield", "pricePlot")
-# Como son 29 provincias, se tiene  que generar cada atributo para cada observacion 
-rvar <- rep(tvar, 29)
-# Que se repitan cada 12 veces
-tvar <- rep(rvar, each = 12)
-dct <- dct %>% mutate(trait =  tvar)
-
-# Retiramos "--" de la columna value
-# Usamos la libreria stringr, la funcion str_replace
-dct$value <- str_replace_all(dct$value, "--", "")
+# Usamos la funcion str_replace_all (paquete stringr)
+# Funcion: str_replace_all(string , pattern, replacement )
+# Parametros
+# string: vector con los valores
+# pattern: el patron que deseamos reemplazar 
+# replacement: el valor por que remplaza al patron.
+# 
+#dcsowa$sowa <- str_replace_all(dcsowa$sowa, "--", "")
 
 
-# En la columna 'month', cambiar X3,...,X14 por el nombre del mes
+# harva: harvest by area  -------------------------------------------------------------------
+# Seleccionamos las siguientes 29 filas: del 31 al 60
+dtharva <- dplyr::slice(dct, 31:60)
+# cabezeras de la tabla harva
+charva <- c("department", "Ene",	"Feb",	"Mar",	"Abr",	"May",	"Jun",	"Jul",	"Ago",	"Set",	"Oct",	
+             "Nov",	 "Dic")
+names(dtharva) <- charva 
 
-# m1: los meses de la primera tabla
-m1 <- c("Ago",	"Set",	"Oct",	"Nov",	"Dic",	"Ene",	"Feb",	"Mar",	"Abr",	"May",	
-          "Jun",	"Jul")
+head(dtharva)
+dtharva <- gather (dtharva, month, harva, Ene:Dic)
 
-# m2: los mese de las tablas restantes
-m2 <- c("Ene",	"Feb",	"Mar",	"Abr",	"May",	"Jun",	"Jul",	"Ago",	"Set",	"Oct",	
-          "Nov",	"Dic")
+# Todavia existe un valor que esta sobrando
+dtharva <- dtharva %>% filter( department!= "harva") %>% arrange(department,month)
 
-month_vector <- rep(m2, 4) 
-month_vector <- c(m1, month_vector)
-# Son 1740 observaciones, tomadas de 60 en 60 ,entonces 1740/60
-month_vector <- rep(month_vector, 29)
-
-# Sustituimos el vector month, por month_vector, 
-# pero manteniendo el nombre "month" en la tabla
-dct <- dct %>% mutate(month =  month_vector)
-
-# Ordenamos por trait
-dct <- dct %>% arrange(trait, month, variable)
-# dct <- dct %>% arrange(month)
-# dct <- dct %>% arrange(variable)
-
-# Agregamos un indice
-dct <- dct %>% mutate(indice = 1:nrow(dct))
-dct <- dct %>% select( variable, indice, month, trait, value)
+#dtharva$harva <- str_replace_all(dtharva$harva, "--", "")
 
 
-# Extraemos ---------------------------------------------------------------
 
-# "variable" "month"    "value"    "trait"    "indice"
-# Para el area de cosecha "harva
+# production --------------------------------------------------------------
 
-#harva <- dct %>% select(trait, value)
-harva <- dct %>% filter(trait == "harva")
-harva <- harva %>% spread(trait, value)
-harva <- harva %>% mutate(year = 2005)
-harva <- harva %>% arrange(variable, month)
-harva <- harva %>% select(-indice)
+# Seleccionamos las siguientes 29 filas: del 31 al 60
+dtproduction <- dplyr::slice(dct, 61:90)
+# cabezeras de la tabla harva
+cproduction <- c("department", "Ene",	"Feb",	"Mar",	"Abr",	"May",	"Jun",	"Jul",	"Ago",	"Set",	"Oct",
+            "Nov",	 "Dic")
+names(dtproduction) <- cproduction
+
+head(dtproduction)
+dtproduction <- gather(dtproduction, month, production, Ene:Dic)
+
+# Todavia existe un valor que esta sobrando
+dtproduction <- dtproduction %>% filter( department!= "production") %>% arrange(department,month)
+
+# yield -------------------------------------------------------------------
+#dtyield <- dplyr::slice(dct, 91:120)
+
+# pricePlot ---------------------------------------------------------------
+
+#dtpricePlot <- dplyr::slice(dct, 121:150)
 
 
-# Para el area de siembre "sowa"
-sowa <- dct %>% filter(trait == "sowa")
-sowa <- sowa %>% spread(trait, value)
-sowa <- sowa %>% mutate(year = 2005)
-sowa <- sowa %>% arrange(variable, month)
-sowa <- sowa %>% select(-indice)
 
-#
+## Como juntar las tablas con left_join
 
-join <- left_join(harva, sowa, by = "month")
+# Usamos la funcino left_join(x , y , by )
+# x : primera tabla
+# y: segunda tabla
+# by: criterio por el cual juntamos las tablas
+
+# out <- left_join(dtsowa, dtharva, by = c("department", "month")) %>% 
+#     left_join(dtproduction, by = c("department", "month"))
+# 
+
+out <- out %>% mutate(year = 2005)
+
+
+# Agregar la data procesada
+
+# wb <- loadWorkbook(fp2)
+# cult_sheet <- "tomate_proc"
+# sheets <- excel_sheets(fp2)
+## if(cult_sheet %in% sheets){
+##   removeWorksheet(wb, cult_sheet)
+## }
+# addWorksheet(wb, "tomate_proc")
+# writeData(wb, sheet = "tomate_proc", x = out)
+# saveWorkbook(wb, file = fp2, overwrite = TRUE)
+
+
+
+
+
 
 
 
