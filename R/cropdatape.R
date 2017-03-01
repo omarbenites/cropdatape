9985f383bd21d8910eaa7954004414e8e3feba8c
omarbenites <omarbenites@github.com>
2017-03-01 15:09:10 -0500

    #Title has changed

--- R/cropdatape.R
+++ R/cropdatape.R
@@ -2,14 +2,14 @@
 #' 
 #' @description The cropdatape package is based on the Integrated System of Agricultural Statistics (SIEA) of Peruvian Agriculture Minestry. 
 #' The variables are: sowing and harvest area, production, crop yield, price per plot by region. The range of year covers from 
-#' 2005 to 2014. Some missing values (\code{NA}) means that in some periods there is no agriculture activity.
+#' 2004 to 2014. Some missing values (\code{NA}) means that in some periods there is no agriculture activity.
 #' 
 #' @format Data frame with --- rows and 9 columns.
 #' \describe{
 #'   \item{crop}{factor with 6 levels}
-#'   \item{region}{Peruvian regions. Factor with 24 levels}
-#'   \item{year}{Numeric. Range of years from 2005 to 2014}
-#'   \item{month}{Factor with 12 levels}
+#'   \item{department}{Peruvian departments. Factor with 24 levels}
+#'   \item{year}{Numeric. Range of years from 2004  to 2014}
+#'   \item{month}{Factor with 12 levels. In case of 2004, it just considered from August to December (mid-seasson).}
 #'   \item{sowa}{Sowing area measured by hectares (ha)}
 #'   \item{harva}{Harvest area measured by hectare (ha)}
 #'   \item{production}{Production of crop measured by tons (t)}
